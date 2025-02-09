8498d7951d9222f8c7f2bb923060546bac4330b5
pablodls04 <pablomiguel200410@gmail.com>
2025-02-09 11:59:28 -0400

    primero

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function helloWorld() {
     return "Hello World";
   }
-  //Pablo de los Santos 2023-0244
+  //Pablo de los Santos Soto 2023-0244
   module.exports = { helloWorld };
   
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const { helloWorld } = require('./index');
-//Pablo de los Santos 2023-0244
+//Pablo de los Santos Soto 2023-0244
 describe('Prueba de helloWorld', () => {
   test('debe retornar "Hello World"', () => {
     expect(helloWorld()).toBe("Hello World");
